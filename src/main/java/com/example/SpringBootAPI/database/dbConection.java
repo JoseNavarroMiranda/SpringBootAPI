8f0dee028b6c57c067c128017b62a8e9035796a5
JoseNavarroMiranda <nqvarr01234@gmail.com>
2026-05-31 20:46:29 -0600

    configuracion de modelos y agregacion de dependencias para conexion a postgress y jpa

--- src/main/java/com/example/SpringBootAPI/database/dbConection.java
+++
@@ -1,6 +0,0 @@
-package com.example.SpringBootAPI.database;
-
-public class dbConection {
-
-
-}
--- src/main/java/com/example/SpringBootAPI/models/Usuario.java
+++ src/main/java/com/example/SpringBootAPI/models/Usuario.java
@@ -1,4 +1,38 @@
 package com.example.SpringBootAPI.models;
+import jakarta.persistence.*;
 
+//condecorador que avisa a springboot que la clase sera una tabla en la db
+@Entity
+@Table(name = "usuario") //esto es opciones para poder dejar el nombre de la tabla
 public class Usuario {
+
+    @Id //se define el campo que sera ID
+    // Condecorador que forza al atributo de la tabla a ser AUTOINCREMENT
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 60)
+    private String nombre;
+
+    @Column(nullable = false, length = 30)
+    private String apellidoPat;
+
+    @Column(nullable = false, length = 30)
+    private String apellidoMat;
+
+    @Column(nullable = false, length = 18)
+    private String password;
+
+    @Column(nullable = true, length = 255)
+    private String fotoPerfil;
+
+    //Creacion de una campo con llave foranea a otra tabla/clase de springboot
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+            name = "rol_id",
+            referencedColumnName = "id",
+            nullable = false,
+    )
+    private rolUsuario rol;
+
 }
